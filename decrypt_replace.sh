34012aa792c8b4314351a8501e2259a18fefd031
Saimanas Gourabathini <gourabathini.s@northeastern.edu>
2025-02-12 23:36:51 -0500

    Assign3

--- decrypt_replace.sh
+++ decrypt_replace.sh
@@ -15,26 +15,43 @@ JENKINS_LOCATION_URL=$(echo "$decrypted_secrets" | grep jenkins_location_url | a
 DOMAIN=$(echo "$decrypted_secrets" | grep domain | awk '{print $2}')
 NGINX_LINK=$(echo "$decrypted_secrets" | grep nginx_link | awk '{print $2}')
 NGINX_CONF=$(echo "$decrypted_secrets" | grep nginx_conf | awk '{print $2}')
-CREDENTIALS_ID=$(echo "$decrypted_secrets" | grep credentials_id | awk '{print $2}')
+REPO_OWNER=$(echo "$decrypted_secrets" | grep repo_owner | awk '{print $2}')
+GITHUB_WEBHOOK_ID=$(echo "$decrypted_secrets" | grep github_webhook_id | awk '{print $2}')
+GITHUB_WEBHOOK_SECRET=$(echo "$decrypted_secrets" | grep github_webhook_password | awk '{print $2}')
+GIT_CREDENTIALS_ID=$(echo "$decrypted_secrets" | grep git_credentials_id | awk '{print $2}')
 GIT_USERNAME=$(echo "$decrypted_secrets" | grep gitUsername | awk '{print $2}')
 GIT_PERSONAL_ACCESS_TOKEN=$(echo "$decrypted_secrets" | grep git_personal_access_token | awk '{print $2}')
-PIPELINE_NAME=$(echo "$decrypted_secrets" | grep pipeline | awk '{print $2}')
-REPO_OWNER=$(echo "$decrypted_secrets" | grep repo_owner | awk '{print $2}')
-REPO_NAME=$(echo "$decrypted_secrets" | grep repo_name | awk '{print $2}')
+TF_GCP_INFRA_PIPELINE_NAME=$(echo "$decrypted_secrets" | grep tf-gcp-infra-pipeline | awk '{print $2}')
+TF_GCP_INFRA_REPO_NAME=$(echo "$decrypted_secrets" | grep tf-gcp-infra-repo_name | awk '{print $2}')
+DOCKER_CREDENTIALS_ID=$(echo "$decrypted_secrets" | grep docker_credentials_id | awk '{print $2}')
+DOCKER_USERNAME=$(echo "$decrypted_secrets" | grep dockerUsername | awk '{print $2}')
+DOCKER_PERSONAL_ACCESS_TOKEN=$(echo "$decrypted_secrets" | grep dockerPersonalAccessToken | awk '{print $2}')
+STATIC_SITE_PIPELINE_NAME=$(echo "$decrypted_secrets" | grep static-site-pipeline | awk '{print $2}')
+STATIC_SITE_REPO_NAME=$(echo "$decrypted_secrets" | grep static-site-repo_name | awk '{print $2}')
 
 sed -i "s/ADMIN_USERNAME=\"\*\*\*\"/ADMIN_USERNAME=\"$ADMIN_USERNAME\"/" jenkins.sh
 sed -i "s/ADMIN_PASSWORD=\"\*\*\*\"/ADMIN_PASSWORD=\"$ADMIN_PASSWORD\"/" jenkins.sh
 sed -i "s/ADMIN_FIRSTNAME=\"\*\*\*\"/ADMIN_FIRSTNAME=\"$ADMIN_FIRSTNAME\"/" jenkins.sh
 sed -i "s/ADMIN_LASTNAME=\"\*\*\*\"/ADMIN_LASTNAME=\"$ADMIN_LASTNAME\"/g" jenkins.sh
 sed -i "s/ADMIN_EMAIL=\"\*\*\*\"/ADMIN_EMAIL=\"$ADMIN_EMAIL\"/" jenkins.sh
-sed -i "s/CREDENTIALS_ID=\"\*\*\*\"/CREDENTIALS_ID=\"$CREDENTIALS_ID\"/" jenkins.sh
-sed -i "s/GIT_USERNAME=\"\*\*\*\"/GIT_USERNAME=\"$GIT_USERNAME\"/" jenkins.sh
-sed -i "s/GIT_PERSONAL_ACCESS_TOKEN=\"\*\*\*\"/GIT_PERSONAL_ACCESS_TOKEN=\"$GIT_PERSONAL_ACCESS_TOKEN\"/" jenkins.sh
-sed -i "s/PIPELINE_NAME=\"\*\*\*\"/PIPELINE_NAME=\"$PIPELINE_NAME\"/" jenkins.sh
 sed -i "s/REPO_OWNER=\"\*\*\*\"/REPO_OWNER=\"$REPO_OWNER\"/" jenkins.sh
-sed -i "s/REPO_NAME=\"\*\*\*\"/REPO_NAME=\"$REPO_NAME\"/" jenkins.sh
 awk -v jenkins_location_url="$JENKINS_LOCATION_URL" '{gsub(/JENKINS_LOCATION_URL="[^"]*"/, "JENKINS_LOCATION_URL=\"" jenkins_location_url "\""); print}' jenkins.sh > jenkins.sh.tmp && mv jenkins.sh.tmp jenkins.sh
 sed -i "s/JENKINS_LOCATION_URL=\"\*\*\*\"/JENKINS_LOCATION_URL=\"$JENKINS_LOCATION_URL\"/" jenkins.sh
 sed -i "s/DOMAIN=\"\*\*\*\"/DOMAIN=\"$DOMAIN\"/" jenkins.sh
 awk -v nginx_conf="$NGINX_CONF" '{gsub(/NGINX_CONF="[^"]*"/, "NGINX_CONF=\"" nginx_conf "\""); print}' jenkins.sh > jenkins.sh.tmp && mv jenkins.sh.tmp jenkins.sh
 awk -v nginx_link="$NGINX_LINK" '{gsub(/NGINX_LINK="[^"]*"/, "NGINX_LINK=\"" nginx_link "\""); print}' jenkins.sh > jenkins.sh.tmp && mv jenkins.sh.tmp jenkins.sh
+
+sed -i "s/GITHUB_WEBHOOK_ID=\"\*\*\*\"/GITHUB_WEBHOOK_ID=\"$GITHUB_WEBHOOK_ID\"/" jenkins.sh
+sed -i "s/GITHUB_WEBHOOK_SECRET=\"\*\*\*\"/GITHUB_WEBHOOK_SECRET=\"$GITHUB_WEBHOOK_SECRET\"/" jenkins.sh
+
+sed -i "s/GIT_CREDENTIALS_ID=\"\*\*\*\"/GIT_CREDENTIALS_ID=\"$GIT_CREDENTIALS_ID\"/" jenkins.sh
+sed -i "s/GIT_USERNAME=\"\*\*\*\"/GIT_USERNAME=\"$GIT_USERNAME\"/" jenkins.sh
+sed -i "s/GIT_PERSONAL_ACCESS_TOKEN=\"\*\*\*\"/GIT_PERSONAL_ACCESS_TOKEN=\"$GIT_PERSONAL_ACCESS_TOKEN\"/" jenkins.sh
+sed -i "s/TF_GCP_INFRA_PIPELINE_NAME=\"\*\*\*\"/TF_GCP_INFRA_PIPELINE_NAME=\"$TF_GCP_INFRA_PIPELINE_NAME\"/" jenkins.sh
+sed -i "s/TF_GCP_INFRA_REPO_NAME=\"\*\*\*\"/TF_GCP_INFRA_REPO_NAME=\"$TF_GCP_INFRA_REPO_NAME\"/" jenkins.sh
+
+sed -i "s/DOCKER_CREDENTIALS_ID=\"\*\*\*\"/DOCKER_CREDENTIALS_ID=\"$DOCKER_CREDENTIALS_ID\"/" jenkins.sh
+sed -i "s/DOCKER_USERNAME=\"\*\*\*\"/DOCKER_USERNAME=\"$DOCKER_USERNAME\"/" jenkins.sh
+sed -i "s/DOCKER_PERSONAL_ACCESS_TOKEN=\"\*\*\*\"/DOCKER_PERSONAL_ACCESS_TOKEN=\"$DOCKER_PERSONAL_ACCESS_TOKEN\"/" jenkins.sh
+sed -i "s/STATIC_SITE_PIPELINE_NAME=\"\*\*\*\"/STATIC_SITE_PIPELINE_NAME=\"$STATIC_SITE_PIPELINE_NAME\"/" jenkins.sh
+sed -i "s/STATIC_SITE_REPO_NAME=\"\*\*\*\"/STATIC_SITE_REPO_NAME=\"$STATIC_SITE_REPO_NAME\"/" jenkins.sh
